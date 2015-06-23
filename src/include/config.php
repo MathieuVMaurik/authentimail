b9899fac708bcd3acf48dfbcf358b2c9b00780f0
Hugo Zink <hugo.zink@hotmail.com>
2015-06-23 12:15:01 +0200

    Fixed corrupt config.

--- src/include/config.php
+++ src/include/config.php
@@ -2,10 +2,6 @@
 
 class Config
 {
-<<<<<<< HEAD
     public static $root_url = 'http://localhost/authentimaildev/'; //The root URL that will be used to construct the authentication and registration emails.
     public static $auth_time_limit = 600; //Authentication time limit in seconds. Default is 600 (10 minutes)
-=======
-    public static $root_url = 'http://localhost/authentimail/src/';
->>>>>>> e60237332001bf17236e2375889c9fb2b51da5be
 }
